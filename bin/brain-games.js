063179500c79026a056d0d5694c05140797e5227
Jerry <mcgerbi4@gmail.com>
2025-03-19 19:57:30 +0300

    Change export to default for single function

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { getName } from '../src/cli.js';
+import getName from '../src/cli.js';
 
 getName();
 console.log('Welcome to the Brain Games!');
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const getName = () => {
+const getName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
+export default getName;
